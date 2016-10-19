beef2f8d674e4f5c66e941b0c776a4ad20aba801
wangmengling <294025529@qq.com>
2016-10-19 09:58:41 +0800

    commit
    commit

--- app/Containers/Admin/Layout/Layout.js
+++ app/Containers/Admin/Layout/Layout.js
@@ -9,11 +9,32 @@ export default class Layout extends Component {
         return(
             <div className='LayoutRoot'>
                 <div className='LayoutTop'>
-                    Top
+                    <div className='TopLogo'>
+                        <img src="/images/logo.png" />
+                    </div>
+                    <div className='TopUser'>
+                        <div className='TopSearch'>
+
+                        </div>
+                        <div className='TopSetting'>
+                            <img src='/images/message.png' />
+                        </div>
+                        <div className='TopSetting'>
+                            <img src='/images/setting.png' />
+                        </div>
+                    </div>
                 </div>
                 <div className='LayoutContent'>
                     <div className='ContentLeft'>
-                        Left
+                        <div className='LeftCatagoryDefault'>
+                            Home
+                        </div>
+                        <div>
+                            User
+                        </div>
+                        <div>
+                            Store
+                        </div>
                     </div>
                     <div className='ContentRight'>
                         {this.props.children}
